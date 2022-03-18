18230c440aafea56a0f7bb3370e13611b5061e6a
AASukhov <arkhonfills@list.ru>
2022-03-18 06:57:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static final int CLIENT_AMOUNT = 10;
+    public static final int CLIENT_COMING = 2000;
+
+    public static void main(String[] args) {
+        final Shop shop = new Shop();
+
+
+        new Thread(null, shop::acceptCar, "Производитель").start();
+
+        for (int i = 1; i <= CLIENT_AMOUNT; i++) {
+            try {
+                new Thread(null, shop::buyCar, "Покупатель " + i).start();
+                Thread.sleep(CLIENT_COMING);
+            } catch (InterruptedException E) {
+                E.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/Seller.java
@@ -0,0 +1,52 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Seller {
+
+    private Shop shop;
+    private static final int RECEIVE_TIME = 1500;
+    private static final int CAR_INPUT = 2000;
+    private final int CARS = 10;
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+
+    public Seller(Shop shop) {
+        this.shop = shop;
+    }
+
+    public synchronized Car buyCar() {
+        System.out.println(Thread.currentThread().getName() + " зашел в автосалон");
+        try {
+            lock.lock();
+            while (shop.cars.size()==0) {
+                System.out.println("Машин нет");
+                condition.await();
+            }
+            Thread.sleep(RECEIVE_TIME);
+            System.out.println("Покупатель " + Thread.currentThread().getName() + " уехал на новеньком авто");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+        return shop.cars.remove(0);
+    }
+
+    public void acceptCar () {
+        for (int i = 0; i < CARS; i++) {
+            try {
+                System.out.println("Производитель toyota выпустил 1 авто");
+                Thread.sleep(CAR_INPUT);
+                shop.cars.add(new Car());
+                lock.lock();
+                condition.signal();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
